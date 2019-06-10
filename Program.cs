30a398a7ba5366316e06be5c1d21e62f55cf8c28
DrPepperMD <51042590+DrPepperMD@users.noreply.github.com>
2019-06-10 17:06:54 +1000

    the source code!

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAHA_REALLY_FUNNY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("|\\ ");
+            Console.WriteLine("|.\\");
+            Console.WriteLine("|..\\ ");
+            Console.WriteLine("|...\\");
+            Console.WriteLine("|...|");
+            Console.WriteLine("|...|");
+            Console.WriteLine("|...|");
+            Console.WriteLine("|...|");
+            Console.WriteLine("|...|");
+            Console.WriteLine("|...|");
+            Console.WriteLine("|___|");
+            Console.WriteLine("|.../");
+            Console.WriteLine("|..|");
+            Console.WriteLine("|..|");
+            Console.WriteLine("|..|");
+            Console.WriteLine("|..|");
+            Console.WriteLine("|___\\");
+            Console.WriteLine("\nget it? this is coded in C#...");
+            Console.WriteLine("and its a knife...");
+
+            Console.WriteLine("\nyou better laugh");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
